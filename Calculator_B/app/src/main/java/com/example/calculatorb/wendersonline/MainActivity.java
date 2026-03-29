dec85fced926a7f3a97e7d525a38061333be40ad
Wenderson Rodrigues Lopes <132304981+wendersonline@users.noreply.github.com>
2026-03-29 16:06:04 -0300

    Exercício B: Calculadora completa com botões e visor

--- Calculator_B/app/src/main/java/com/example/calculatorb/wendersonline/MainActivity.java
+++ Calculator_B/app/src/main/java/com/example/calculatorb/wendersonline/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.calculatorb.wendersonline;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
+    EditText visor;
+    String operator = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,95 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        visor = (EditText) findViewById(R.id.visor);
+    }
+
+    public void pressNumber(View v) {
+        String lastNums = visor.getText().toString();
+        if (!lastNums.equals(getString(R.string.error))) {
+            String newNums = lastNums + v.getTag().toString();
+            visor.setText(newNums);
+        } else {
+            visor.setText(v.getTag().toString());
+        }
+    }
+
+    public void pressDot(View v) {
+        String lastNums = visor.getText().toString();
+
+        if (lastNums.matches(".*\\d$") && !lastNums.matches(".*\\.[^+\\-*/]*$")) {
+            String newNums = lastNums + v.getTag().toString();
+            visor.setText( newNums );
+        }
+    }
+
+    public void pressOperator(View v) {
+        String lastNums = visor.getText().toString();
+
+        if (!lastNums.isEmpty() && !lastNums.equals(getString(R.string.error)) && !lastNums.matches(".*[+\\-*/].*")) {
+            String newNums = lastNums + v.getTag().toString();
+            visor.setText( newNums );
+            operator = v.getTag().toString();
+        }
+    }
+
+    public void eraseNumber(View v) {
+        if (v.getTag().toString().equals("C")) {
+            visor.setText(" ");
+        } else  {
+            String lastNums = visor.getText().toString();
+            if (!lastNums.isEmpty()) {
+                lastNums = lastNums.substring(0, lastNums.length() - 1);
+            }
+
+            visor.setText(lastNums);
+        }
+    }
+
+    public void pressEqual(View v) {
+        String allOperation = visor.getText().toString();
+
+        if (allOperation.isEmpty()) {
+            visor.setText(R.string.error);
+            return;
+        }
+
+        String[] nums = allOperation.split(Pattern.quote(operator));
+
+        if (nums.length < 2) {
+            visor.setText(R.string.error);
+            return;
+        }
+
+        double firstValue = Double.parseDouble(nums[0]);
+        double secondValue = Double.parseDouble(nums[1]);
+        double resultValue = 0;
+
+        switch (operator) {
+            case "+":
+                resultValue = firstValue + secondValue;
+                break;
+
+            case "-":
+                resultValue = firstValue - secondValue;
+                break;
+
+            case "*":
+                resultValue = firstValue * secondValue;
+                break;
+
+            case "/":
+                if (secondValue == 0) {
+                    visor.setText(getString(R.string.error));
+                    return;
+                }
+                else {
+                    resultValue = (double) firstValue / secondValue;
+                    break;
+                }
+        }
+
+        visor.setText(String.valueOf(resultValue));
     }
 }
